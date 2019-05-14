4527a79e75f05bb927ea1c0f4bff1dda89084a62
Mega Senlian Jenny <177006018@student.unsil.ac.id>
2019-05-14 23:59:54 +0700

    upload

---
+++ about.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Malindo Outdoor - Rent & Laundy</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                   
+                </button>
+                <a class="navbar-brand" href="index.php">Malindo Outdoor</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+					<li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Sewa | Login<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="Login.php">Admin</a>
+                            </li>
+                            <li>
+                                <a href="Login_Member.php">Member</a>
+                            </li>
+                            <li>
+                                <a href="register.php">Sign Up</a>
+                            </li>
+                        </ul>
+                    </li>
+					<li>
+                        <a href="about.php">Tentang Kami</a>
+                    </li>
+					<li>
+                        <a href="contact.php">Kontak</a>
+                    </li>        
+                </ul>
+
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Tentang
+                    <small>Kami</small>
+                </h1>
+                <ol class="breadcrumb">
+                    <li><a href="index.php">Beranda</a>
+                    </li>
+                    <li class="active">Tentang Kami</li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row">
+            <div class="col-md-6">
+                <img class="img-responsive" src="foto/malindo.png" alt="">
+            </div>
+            <div class="col-md-6">
+              <h2>Malindo Outdoor</h2>
+                <p>Malindo  diambil dari singkatan Tasikmalaya dan Indonesia yang artinya perusahaan ini  berdiri atau berasal dari Tasikmalaya Indonesia dengan harapan Malindo bisa  membawa nama Tasikmalaya ke kancah nasional (Indonesia). Malindo merupakan  suatu perusahaan yang bergerak dibidang penyewaan alat-alat kemping, pencucian  alat-alat kemping, kegiatan perkemahan siswa, mahasiswa, organisasi, pendakian  gunung, kemping bersama dan berbagai kegiatan yang bergerak di <em>Outdoor</em>.<br>
+                  Berawal  dari sebuah kegiatan pertualangan generasi muda yang tergabung dalam komunitas  pecinta alam terutama pendakian gunung dan penjelajahan rimba. Aktifitas serupa  merupakan adalah kegiatan yang rutin dilakukan setiap bulannya seperti  pendakian gunung hingga kemping bersama. Peralatan pendakian merupakan hal yang  penting seperti tenda, <em>flysheet</em>, <em>cooking set</em>, <em>sleeping bag</em>, dll.<br>
+                Didasari  atas keinginan untuk memenuhi keubutuhan peralatan <em>outdoor  </em>atau pendakian bagi  para penggiat jelajah alam, terbentuklah perusahaan Malindo Tasikmalaya.</p>
+</div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Team Members -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Our Team</h2>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="foto/ega.png" alt="">
+                    <div class="caption">
+                        <h3>Mega Senlian Jenny<br>
+                            <small>Admin</small>
+                        </h3>
+                        <p>Seorang yang mengatur jadwal kegiatan alam bebas dan salah satu anggota Organisasi Pecinta Alam</p>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="foto/p.jpg" alt="">
+                    <div class="caption">
+                        <h3>Peri Saepullah<br>
+                            <small>Admin</small>
+                        </h3>
+                        <p>Seorang yang mengrekomendasikan kegitan alam bebas dan anggota dari Organisasi Pecinta Alam.</p>
+                        
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img src="foto/i.jpeg" alt="" width="354" height="406" class="img-responsive">
+                    <div class="caption">
+                        <h3>Muhammad Irfan Mujaddid<br>
+                            <small>Admin</small>
+                        </h3>
+                        <p>Seorang yang mengrekomendasikan dan pengiat kegiatan alam bebas dan yang sangat mencintai kegiatan alam bebas.</p>
+                </div>
+            </div>
+        </div>
+    <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img class="img-responsive" src="foto/q.jpeg" alt="">
+                    <div class="caption">
+                        <h3>Fikri Maulana Ikhsan<br>
+                            <small>Admin</small>
+                        </h3>
+                        <p>Seorang yang mengingaat kegiatan alam bebas dan yang sangat mencintai kegiatan alam bebas</p>
+                        
+                    </div>
+                </div>
+            </div> 
+        <div class="col-md-4 text-center">
+                <div class="thumbnail">
+                    <img src="foto/r.jpeg" alt="" width="354" height="406" class="img-responsive">
+                    <div class="caption">
+                        <h3>Windu Rahayu<br>
+                            <small>Admin</small>
+                        </h3>
+                        <p>Seorang yang mengingaat kegiatan alam bebas dan yang sangat mencintai kegiatan alam bebas.</p>
+                           
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- /.row -->
+
+        <!-- Our Customers -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Our Customers</h2>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="foto/1.jpg" alt="" >
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="foto/2.jpg" alt="" >
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="foto/3.jpg" alt="" >
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="foto/4.jpg" alt="" >
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="foto/5.jpg" alt="" >
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="foto/6.jpg" alt="" >
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>IMK &copy;2019 Malindo Outdoor, mega, peri, irfan, fikri, windu </p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
